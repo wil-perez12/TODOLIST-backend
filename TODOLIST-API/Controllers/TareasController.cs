947b1714b61c40cfcc880572ab94e1eeebf2d471
ing Wilnel <perezwilnel12@gmail.com>
2025-03-12 10:35:16 -0400

    add: comentarios a los enpoints de tareas

--- TODOLIST-API/Controllers/TareasController.cs
+++ TODOLIST-API/Controllers/TareasController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using TODO.Interfaces;
 using TODO.Models.Dtos;
 
@@ -19,6 +20,14 @@ namespace TODO.Controllers
         }
 
         [HttpGet("ListaTareas")]
+
+
+        [SwaggerOperation(
+        Summary = "Lista de tareas",
+        Description = "Devuelve una lista de tareas"
+        )]
+        [SwaggerResponse(200, "Devuelve la lista")]
+        [SwaggerResponse(401, "no autorizado si no estas autenticado")]
         public async Task<IActionResult> GetTareas()
         {
             var lista = await _servicio.GetTarea();
@@ -26,6 +35,14 @@ namespace TODO.Controllers
         }
 
         [HttpGet("TareasPor/{id}")]
+
+        [SwaggerOperation(
+        Summary = "Lista de tareas por ID",
+        Description = "obtiene el id de la tarea"
+        )]
+        [SwaggerResponse(200, "Devuelve la lista")]
+        [SwaggerResponse(400, "Devuelve: Tarea no encontrada con este ID")]
+        [SwaggerResponse(401, "no autorizado si no estas autenticado")]
         public async Task<IActionResult> GetTareaById(int id)
         {
             var lista = await _servicio.GetTareaById(id);
@@ -38,6 +55,14 @@ namespace TODO.Controllers
         }
 
         [HttpGet("TareasBy/{estado}")]
+
+        [SwaggerOperation(
+        Summary = "Lista de tareas por estado",
+        Description = "obtiene el estado de la tarea"
+        )]
+        [SwaggerResponse(200, "Devuelve la lista")]
+        [SwaggerResponse(400, "Devuelve: Tarea no encontrada con este estado")]
+        [SwaggerResponse(401, "no autorizado si no estas autenticado")]
         public async Task<IActionResult> GetTareaById(string estado)
         {
             var lista = await _servicio.GetTareaByEstado(estado);
@@ -50,6 +75,14 @@ namespace TODO.Controllers
         }
 
         [HttpPost("New/tarea")]
+
+        [SwaggerOperation(
+        Summary = "Crear Tarea",
+        Description = "obtiene el titulo, descripcion, estado y ID-usuario"
+        )]
+        [SwaggerResponse(200, "Devuelve la tarea creada")]
+        [SwaggerResponse(500, "Error al crear la tarea!")]
+        [SwaggerResponse(401, "no autorizado si no estas autenticado")]
         public async Task<IActionResult> PostTarea(TareaDTO modelo)
         {
             var newTarea = await _servicio.PostTarea(modelo);
@@ -62,6 +95,14 @@ namespace TODO.Controllers
         }
 
         [HttpPut("update/tarea")]
+
+        [SwaggerOperation(
+        Summary = "Modificar Tarea",
+        Description = "obtiene el id, titulo, descripcion, estado y ID-usuario"
+        )]
+        [SwaggerResponse(200, "Devuelve la tarea modificada")]
+        [SwaggerResponse(400, "Id incorrecto o error al actualizar la tarea")]
+        [SwaggerResponse(401, "no autorizado si no estas autenticado")]
         public async Task<IActionResult> PutTarea(int id, TareaDTO modelo)
         {
             var updateTarea = await _servicio.PutTarea(id, modelo);
@@ -74,6 +115,14 @@ namespace TODO.Controllers
         }
 
         [HttpDelete("delete")]
+
+        [SwaggerOperation(
+        Summary = "Eliminar Tarea",
+        Description = "obtiene el id de la tarea"
+        )]
+        [SwaggerResponse(200, "Tarea con ID eliminada!")]
+        [SwaggerResponse(400, "Id no encontrado")]
+        [SwaggerResponse(401, "no autorizado si no estas autenticado")]
         public async Task<IActionResult> DeleteTarea(int id)
         {
             var extisteID = await _servicio.DeleteTarea(id);
